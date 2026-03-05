8a9c0ebc521c514371e5938b785f9ba3a8bfe9f5
Hanna Kyriukhina <anne.snab+hanna1206@gmail.com>
2026-03-05 10:10:19 +0100

    feat: initiate form-builder module

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App.tsx';
+import { FormBuilder } from './modules/form-builder/components/form-builder';
+
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <FormBuilder />
   </StrictMode>,
 );
